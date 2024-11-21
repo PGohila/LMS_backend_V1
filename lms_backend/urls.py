33bd633977bc1554da8e6d814cf7540291042a0f
Mathan-dd15 <mathan.dd08@gmail.com>
2024-11-21 18:27:30 +0530

    user management

--- lms_backend/urls.py
+++ lms_backend/urls.py
@@ -20,9 +20,11 @@ from rest_framework_simplejwt.views import ( TokenObtainPairView,TokenRefreshVie
 from django.conf.urls.static import static
 from django.conf import settings
 from django.urls import include
+from mainapp.views import login_view
 
 urlpatterns = [
     path('', include('mainapp.urls')),
+    path('api/token/', login_view),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
